263124a6ed4c39899e16d8f1869c4d61dc833f17
Nitin Gupta <nitingupta03012000@gmail.com>
2020-08-21 20:45:15 +0530

    Update Sort Array By Parity  Solution.cpp

--- LeetCode/Aug_Long_2020/Sort Array By Parity  Solution.cpp
+++ LeetCode/Aug_Long_2020/Sort Array By Parity  Solution.cpp
@@ -7,16 +7,23 @@ public:
             return A;
         }
         int even=0,odd=A.size()-1;
+        
         while(even<odd&&even<A.size()&&odd>=0)
         {
-            while(even<A.size()&&A[even]%2!=1) even++;
-            while(odd>=0&&A[odd]%2!=0) odd--;
-            if(even<odd&&odd>=0&&even<A.size())
+            while(even<odd && A[even]%2!=1) even++;
+            
+            while(odd>even&&A[odd]%2!=0) odd--;
+            
+            if(even<odd)
             {
                 swap(A[even],A[odd]);
             }
+            
             even++;odd--;
         }
+        
         return A;
+        
     }
+    
 };
